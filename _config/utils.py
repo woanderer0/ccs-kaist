2958fbcf25f661685b0b72390e8d1d43d5c08aaf
killerwhalee <killerwhalee@kaist.ac.kr>
2023-11-11 03:49:12 +0000

    Added email verification token generator

--- _config/utils.py
+++ _config/utils.py
@@ -3,6 +3,7 @@ import os
 from uuid import uuid4
 from django.utils import timezone
 
+# Encrypting file name
 def uuid_filepath(instance, filename):
     app_name = instance.__class__._meta.app_label
     model_name = instance.__class__.__name__.lower()
@@ -13,3 +14,15 @@ def uuid_filepath(instance, filename):
         model_name,
         uuid_name + extension,
     ])
+
+# Generating email verification token
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+import six
+
+class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return (six.text_type(user.pk) + six.text_type(timestamp)) + six.text_type(user.is_active)
+
+account_activation_token = AccountActivationTokenGenerator()
+
+#
