1ec01d221d0e718651588f96073ec15f23954167
killerwhalee <killerwhalee@kaist.ac.kr>
2023-11-11 03:52:04 +0000

    Fixed minor problems

--- common/forms.py
+++ common/forms.py
@@ -2,8 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-
-class UserForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
     email = forms.EmailField(label="email")
 
     class Meta:
