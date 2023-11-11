311bee4d597ae79dafee1d4310adbbde1eae2a83
killerwhalee <killerwhalee@kaist.ac.kr>
2023-11-11 17:23:18 +0000

    Renamed username field due to confusion

--- medass/models.py
+++ medass/models.py
@@ -5,6 +5,6 @@ from _config.utils import uuid_filepath
 
 # Create your models here.
 class MedassData(models.Model):
-    username = models.ForeignKey(User, verbose_name="username", on_delete=models.PROTECT)
+    user = models.ForeignKey(User, verbose_name="username", on_delete=models.PROTECT)
     date_created = models.DateTimeField("date created", auto_now=False, auto_now_add=True)
     csv_data = models.FileField("csv data file", upload_to=uuid_filepath, max_length=None, null=False)
