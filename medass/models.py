9f8beed3c72fb01530c77139c0da3f2fe4ca39f4
killerwhalee <killerwhalee@kaist.ac.kr>
2024-01-02 12:16:56 +0000

    Override delete for csv file

--- medass/models.py
+++ medass/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from _config.settings.base import MEDIA_ROOT
 from _config.utils import uuid_filepath
 
 class MedassData(models.Model):
+
+    # Override delete() to delete connected csv file
+    def delete(self, *args, **kargs):
+        import os
+        if self.csv_data:
+            os.remove(os.path.join(MEDIA_ROOT, self.csv_data.path))
+        super(MedassData, self).delete(*args, **kargs)
+
     user = models.ForeignKey(User, verbose_name="username", on_delete=models.PROTECT)
     date_created = models.DateTimeField("date created", auto_now=False, auto_now_add=True)
     csv_data = models.FileField("csv data file", upload_to=uuid_filepath, max_length=None, null=False)
